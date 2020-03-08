8572fe8848a08059de0361af2e9fb0bd883ab6af
Vadym Hutei <hutei@live.com>
2020-03-08 23:11:36 +0200

    [REBUILD]

--- rebuild.sh
+++ rebuild.sh
@@ -3,5 +3,6 @@
 docker stop lifetime_app
 docker rm lifetime_app
 docker rmi lifetime_app
+git pull origin master
 docker build -t lifetime_app .
 docker run --name lifetime_app -d --restart always -p 80:80 lifetime_app
