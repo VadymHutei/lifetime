13247c149f0f08ea89404b6e6047df5e3f3dc2ca
Vadym Hutei <hutei@live.com>
2020-03-11 20:36:15 +0200

    [mysql] temp turned off

--- rebuild.sh
+++ rebuild.sh
@@ -4,12 +4,12 @@ docker stop lifetime_app
 docker rm lifetime_app
 docker rmi lifetime_app
 
-docker stop mysql
-docker rm mysql
+# docker stop mysql
+# docker rm mysql
 
 git pull origin master
 
 docker build -t lifetime_app .
 docker run --name lifetime_app -d --restart always -p 80:80 lifetime_app
 
-docker run --name mysql -d --restart always -v /home/mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=StrongPass -p 3306:3306 mysql:5
+# docker run --name mysql -d --restart always -v /home/mysql_data:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=StrongPass -p 3306:3306 mysql:5
