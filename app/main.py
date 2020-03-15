133a44fe0d2e56a56a2cc138659f1df24a399c8a
Vadym Hutei <hutei@live.com>
2020-03-15 21:39:37 +0200

    [TRANSLATIONS]

--- app/main.py
+++ app/main.py
@@ -41,14 +41,88 @@ def lang_redirect(f):
     return decorated_function
 
 
+@app.route('/translations', methods=['GET'])
+def translations_page():
+    admin_key = request.args.get('admin_key')
+    if admin_key != config.admin_key:
+        abort(400)
+    params = {
+        'admin_key': admin_key,
+        'languages': languages,
+        'translations': translations
+    }
+    return render_template('translations.html', **params)
+
+@app.route('/translations/add', methods=['GET', 'POST'])
+def translations_add():
+    admin_key = request.args.get('admin_key')
+    if admin_key != config.admin_key:
+        abort(400)
+    if request.method == 'GET':
+        params = {
+            'admin_key': admin_key,
+            'languages': languages
+        }
+        return render_template('translations_add.html', **params)
+    if request.method == 'POST':
+        code = request.form['code']
+        translations = {}
+        for name, translation in request.form.items():
+            if name[:9] == 'translate':
+                language = name[10:13]
+                if language not in languages:
+                    continue
+                translations[language] = translation
+        lt_lib.setTranslations(code, translations)
+        return redirect(url_for('translations_page', admin_key=config.admin_key))
+
+@app.route('/translations/edit', methods=['GET', 'POST'])
+def translations_edit():
+    admin_key = request.args.get('admin_key')
+    if admin_key != config.admin_key:
+        abort(400)
+    global translations
+    if request.method == 'GET':
+        code = request.args.get('code')
+        params = {
+            'admin_key': admin_key,
+            'languages': languages,
+            'code': code,
+            'translations': translations[code]
+        }
+        return render_template('translations_edit.html', **params)
+    if request.method == 'POST':
+        code = request.form['code']
+        trans = {}
+        for name, translation in request.form.items():
+            if name[:9] == 'translate':
+                language = name[10:13]
+                if language not in languages:
+                    continue
+                trans[language] = translation
+        lt_lib.setTranslations(code, trans)
+        translations = lt_lib.getTranlations()
+        return redirect(url_for('translations_page', admin_key=config.admin_key))
+
+@app.route('/translations/delete', methods=['GET'])
+def translations_delete():
+    admin_key = request.args.get('admin_key')
+    if admin_key != config.admin_key:
+        abort(400)
+    code = request.args.get('code')
+    lt_lib.deleteTranslation(code)
+    global translations
+    translations = lt_lib.getTranlations()
+    return redirect(url_for('translations_page', admin_key=config.admin_key))
+
 @app.route('/', methods=['GET'])
-@app.route('/<language>/', methods=['GET'])
+@app.route('/<language>', methods=['GET'])
 @lang_redirect
 def main(language=default_language):
     params = {
         'translate': lt_lib.getTranslator(translations, language),
         'language': language,
-        'translations': {t: v[language] for t, v in translations.items()},
+        'translations': {t: v.get(language) for t, v in translations.items()},
         'countries': countries,
         'regions': regions,
         'world': world,
