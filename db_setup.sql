fd079227e99006b8828d7b63961b9fe5227e4d01
Vadym Hutei <hutei@live.com>
2020-03-15 19:11:56 +0200

    [DB BACKUP]

---
+++ db_setup.sql
@@ -0,0 +1,118 @@
+-- --------------------------------------------------------
+-- Сервер:                       lifetime.hutei.net
+-- Версія сервера:               5.7.29 - MySQL Community Server (GPL)
+-- ОС сервера:                   Linux
+-- HeidiSQL Версія:              10.3.0.5771
+-- --------------------------------------------------------
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+
+
+-- Dumping database structure for lifetime
+CREATE DATABASE IF NOT EXISTS `lifetime` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci */;
+USE `lifetime`;
+
+-- Dumping structure for таблиця lifetime.countries
+CREATE TABLE IF NOT EXISTS `countries` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `overall` float NOT NULL,
+  `female` float NOT NULL,
+  `male` float NOT NULL,
+  `type` enum('country','region') COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `name` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=188 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.countries_text
+CREATE TABLE IF NOT EXISTS `countries_text` (
+  `country_id` int(10) unsigned NOT NULL,
+  `lang` char(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(64) CHARACTER SET utf8 COLLATE utf8_unicode_ci NOT NULL,
+  PRIMARY KEY (`country_id`,`lang`),
+  KEY `FK_countries_text_languages` (`lang`),
+  CONSTRAINT `FK_countries_text_countries` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_countries_text_languages` FOREIGN KEY (`lang`) REFERENCES `languages` (`code`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.genders
+CREATE TABLE IF NOT EXISTS `genders` (
+  `id` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.genders_text
+CREATE TABLE IF NOT EXISTS `genders_text` (
+  `gender_id` tinyint(3) unsigned NOT NULL,
+  `lang` char(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`gender_id`,`lang`),
+  KEY `FK_sex_text_languages` (`lang`),
+  CONSTRAINT `FK_genders_text_genders` FOREIGN KEY (`gender_id`) REFERENCES `genders` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_sex_text_languages` FOREIGN KEY (`lang`) REFERENCES `languages` (`code`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.languages
+CREATE TABLE IF NOT EXISTS `languages` (
+  `code` char(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `name` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `default` enum('Y','N') COLLATE utf8mb4_unicode_ci NOT NULL DEFAULT 'N',
+  PRIMARY KEY (`code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.life_exp
+CREATE TABLE IF NOT EXISTS `life_exp` (
+  `country_id` int(10) unsigned NOT NULL,
+  `gender_id` tinyint(3) unsigned NOT NULL,
+  `life_exp` float unsigned NOT NULL,
+  PRIMARY KEY (`country_id`,`gender_id`),
+  KEY `life_exp` (`life_exp`),
+  KEY `FK_life_exp_genders` (`gender_id`),
+  CONSTRAINT `FK_life_exp_countries` FOREIGN KEY (`country_id`) REFERENCES `countries` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_life_exp_genders` FOREIGN KEY (`gender_id`) REFERENCES `genders` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.log
+CREATE TABLE IF NOT EXISTS `log` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `ip` varchar(15) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `url` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `method` varchar(8) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `date` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `ip` (`ip`)
+) ENGINE=InnoDB AUTO_INCREMENT=144 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+-- Dumping structure for таблиця lifetime.translations
+CREATE TABLE IF NOT EXISTS `translations` (
+  `code` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `lang` char(3) COLLATE utf8mb4_unicode_ci NOT NULL,
+  `translate` varchar(64) COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`code`,`lang`),
+  KEY `FK_translations_languages` (`lang`),
+  CONSTRAINT `FK_translations_languages` FOREIGN KEY (`lang`) REFERENCES `languages` (`code`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+-- Дані для експорту не вибрані
+
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
