914d78a5d8bc0450b66273ff1e186757f50162a2
Vadym Hutei <hutei@live.com>
2020-03-21 22:08:10 +0200

    [HOME]

--- app/main.py
+++ app/main.py
@@ -2,23 +2,13 @@ from datetime import date, datetime
 import math
 from functools import wraps
 
-from flask import (
-    Flask,
-    render_template,
-    request,
-    abort,
-    redirect,
-    url_for,
-    send_from_directory
-)
+from flask import (Flask, render_template, request, abort, redirect, url_for,
+    send_from_directory)
 from dateutil.relativedelta import relativedelta
 
 import config
 import data
 import lt_lib
-
-def foo():
-    return 'bar'
 
 
 app = Flask(__name__)
@@ -29,6 +19,7 @@ translations = lt_lib.getTranlations()
 
 # countries
 countries = lt_lib.getCountries()
+world = countries[181]
 country_aliases = {country['alias']: country['id'] for _, country in countries.items()}
 
 # genders
@@ -42,12 +33,122 @@ def lang_redirect(f):
         if language:
             if language not in languages:
                 return redirect(url_for(
-                    'main',
+                    'home',
                     language=default_language,
                     **request.args
                 ))
         return f(*args, **kwargs)
     return decorated_function
+
+
+@app.route('/', methods=['GET'])
+def root():
+    return redirect(url_for('home', language=default_language, **request.args))
+
+
+@app.route('/<string:language>', methods=['GET'])
+@lang_redirect
+def home(language=default_language):
+    params = {
+        'site_addr': f'{request.scheme}://{request.host}',
+        'languages': languages,
+        'language': language,
+        'translations': {t: v.get(language) for t, v in translations.items()},
+        'world': world,
+        'life_exp': lt_lib.decomposeDate(years=world['life_exp'][1]),
+        'genders': genders
+    }
+    params.update({
+        'translate': lt_lib.getTranslator(translations, language),
+        'dateFormate': lt_lib.getDateFormator(language=language)
+    })
+    return render_template('pages/home.html', **params)
+
+
+@app.route('/<string:language>/result', methods=['GET'])
+@lang_redirect
+def result(language=default_language):
+    birth_date = request.args.get('birth_date')
+    if not birth_date:
+        abort(400)
+    birth_date = datetime.strptime(birth_date, '%Y-%m-%d').date()
+
+    gender = int(request.args.get('sex', '1'))
+    if gender not in genders:
+        abort(400)
+
+    country_id = int(request.args.get('country', '181'))
+    if country_id not in countries:
+        abort(400)
+    country = countries.get(country_id)['name'][language]
+    life_exp = countries.get(country_id)['life_exp']
+
+    life_length_years = math.trunc(life_exp[gender])
+    life_length_months = round(12 * math.modf(life_exp[gender])[0])
+    life_length = relativedelta(
+        years=life_length_years,
+        months=life_length_months
+    )
+
+    today_date = date.today()
+
+    death_date = birth_date + life_length
+
+    lived = relativedelta(birth_date, today_date)
+
+    life_left = relativedelta(death_date, today_date)
+
+    params = {
+        'site_addr': f'{request.scheme}://{request.host}',
+        'languages': languages,
+        'translate': lt_lib.getTranslator(translations, language),
+        'today_date': today_date.strftime('%d %b %Y'),
+        'birth_date': birth_date.strftime('%d %b %Y'),
+        'life_length': {
+            'years': life_length_years,
+            'months': life_length_months
+        },
+        'death_date': death_date.strftime('%d %b %Y'),
+        'life_left': {
+            'years': life_left.years,
+            'months': life_left.months,
+            'days': life_left.days
+        },
+        'sex': genders[gender][language]['name'],
+        'country': country,
+        'lived': {
+            'years': abs(lived.years),
+            'months': abs(lived.months)
+        }
+    }
+
+    return render_template('pages/result.html', **params)
+
+
+@app.route('/<string:language>/<string:alias>', methods=['GET'])
+@lang_redirect
+def main_country(alias, language=default_language):
+    if alias in country_aliases:
+        country_id = country_aliases[alias]
+        country = countries.get(country_id)
+        if country is None:
+            abort(404)
+        params = {
+            'translate': lt_lib.getTranslator(translations, language),
+            'language': language,
+            'translations': {t: v.get(language) for t, v in translations.items()},
+            'country': country,
+            'genders': genders
+        }
+        return render_template('country.html', **params)
+    else:
+        lt_lib.log(
+            request.remote_addr,
+            f'/{language}/{alias}',
+            request.method,
+            datetime.now()
+        )
+        abort(404)
 
 
 @app.route('/translations', methods=['GET'])
@@ -128,109 +229,6 @@ def translations_delete():
     global translations
     translations = lt_lib.getTranlations()
     return redirect(url_for('translations_page', admin_key=config.admin_key))
-
-
-@app.route('/', methods=['GET'])
-def root():
-    return redirect(url_for('main', language=default_language, **request.args))
-
-
-@app.route('/<string:language>', methods=['GET'])
-@lang_redirect
-def main(language=default_language):
-    params = {
-        'translate': lt_lib.getTranslator(translations, language),
-        'language': language,
-        'translations': {t: v.get(language) for t, v in translations.items()},
-        'countries': countries,
-        'world': countries[181],
-        'genders': genders
-    }
-    return render_template('main.html', **params)
-
-
-@app.route('/<string:language>/result', methods=['GET'])
-@lang_redirect
-def result(language=default_language):
-    birth_date = request.args.get('birth_date')
-    if not birth_date:
-        abort(400)
-    birth_date = datetime.strptime(birth_date, '%Y-%m-%d').date()
-
-    gender = int(request.args.get('sex', '1'))
-    if gender not in genders:
-        abort(400)
-
-    country_id = int(request.args.get('country', '181'))
-    if country_id not in countries:
-        abort(400)
-    country = countries.get(country_id)['name'][language]
-    life_exp = countries.get(country_id)['life_exp']
-
-    life_length_years = math.trunc(life_exp[gender])
-    life_length_months = round(12 * math.modf(life_exp[gender])[0])
-    life_length = relativedelta(
-        years=life_length_years,
-        months=life_length_months
-    )
-
-    today_date = date.today()
-
-    death_date = birth_date + life_length
-
-    lived = relativedelta(birth_date, today_date)
-
-    life_left = relativedelta(death_date, today_date)
-
-    params = {
-        'translate': lt_lib.getTranslator(translations, language),
-        'today_date': today_date.strftime('%d %b %Y'),
-        'birth_date': birth_date.strftime('%d %b %Y'),
-        'life_length': {
-            'years': life_length_years,
-            'months': life_length_months
-        },
-        'death_date': death_date.strftime('%d %b %Y'),
-        'life_left': {
-            'years': life_left.years,
-            'months': life_left.months,
-            'days': life_left.days
-        },
-        'sex': genders[gender][language]['name'],
-        'country': country,
-        'lived': {
-            'years': abs(lived.years),
-            'months': abs(lived.months)
-        }
-    }
-
-    return render_template('result.html', **params)
-
-
-@app.route('/<string:language>/<string:alias>', methods=['GET'])
-@lang_redirect
-def main_country(alias, language=default_language):
-    if alias in country_aliases:
-        country_id = country_aliases[alias]
-        country = countries.get(country_id)
-        if country is None:
-            abort(404)
-        params = {
-            'translate': lt_lib.getTranslator(translations, language),
-            'language': language,
-            'translations': {t: v.get(language) for t, v in translations.items()},
-            'country': country,
-            'genders': genders
-        }
-        return render_template('country.html', **params)
-    else:
-        lt_lib.log(
-            request.remote_addr,
-            f'/{language}/{alias}',
-            request.method,
-            datetime.now()
-        )
-        abort(404)
 
 
 @app.route('/robots.txt')
